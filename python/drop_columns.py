d7f58ced06802de5a82122715ec26ecd9531f58e
David Remenyik <david.remenyik@ucdconnect.ie>
2026-02-12 15:13:06 +0000

    Moved python scripts to Python folder and csv to Data folder updated output of the scripts

--- python/drop_columns.py
+++ python/drop_columns.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("name_extraction.csv")
 df.drop(columns=["index", "advice"], inplace=True, errors='ignore')
-df.to_csv("cleaned_file.csv", index=False)
+df.to_csv("data/cleaned_file.csv", index=False)
